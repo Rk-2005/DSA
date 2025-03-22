ada88ada1e99306958da326731c9b9440efbec06
Ritesh Kriplani <kriplanira@rknec.edu>
2025-03-22 17:49:37 +0530

    Time: 36 ms (79.58%), Space: 132.1 MB (58.2%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,74 +1,35 @@
 class Solution {
 public:
-    vector<int>parent,rank;
-    int find(int x){
-        if(x==parent[x]){
-            return x;
-        }
-
-        return parent[x]=find(parent[x]);
-    }
-    void unionn(int a,int b){
-        int x=find(a);
-        int y=find(b);
-        if(x==y){
-            return;
-        }else if(rank[x]>rank[y]){
-            rank[x]++;
-            parent[y]=x;
-        }else{
-            rank[y]++;
-            parent[x]=y;
+    void dfs(int src,vector<bool>&vis,int &v,int &e,unordered_map<int,vector<int>>&mp){
+        v++;
+        e+=mp[src].size();
+        vis[src]=1;
+        for(auto it:mp[src]){
+            if(vis[it]==false){
+                dfs(it,vis,v,e,mp);
+            }
         }
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        
-        parent.resize(n);
-        rank.resize(n);
+        unordered_map<int,vector<int>>mp;
+        for(auto &it:edges){
+            int x=it[0],y=it[1];
+            mp[x].push_back(y);
+            mp[y].push_back(x);
+        }
+        int ans=0;
+        vector<bool>vis(n,0);
         for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
-        unordered_map<int, vector<int>> mp;
-        for (auto& it : edges) {
-            unionn(it[0],it[1]);
-            mp[it[0]].push_back(it[1]);
-            mp[it[1]].push_back(it[0]);
-            
-        }
-        for(int i=0;i<n;i++)
-        {
-            parent[i]=find(i);
-        }
-        int ans = 0;
-        map<int,int>mp2;
-        for(int i=0;i<n;i++){
-            mp2[parent[i]]++;
-        }
-        vector<int>ab(n,1);
-        for(int i=0;i<n;i++){
-            int p=parent[i];
-            if(mp2[p]!=-1 && (mp2[p]-1)!=mp[i].size()){
-               mp2[p]=-1;
+            if(vis[i]==false){
+                int v=0;
+                int e=0;
+                dfs(i,vis,v,e,mp);
+                if(v*(v-1)==e){
+                    ans++;
+                }
             }
-        }
-        for(auto it:mp2){
-           if(it.second!=-1){
-            ans++;
-           }
         }
         return ans;
     }
 };
 
-/// dsu
-
-// 0 1 2
-// 1 2 2 
-
-// 2
-// |
-// 3
-// |
-// 1 ->
-// |
-// 0
