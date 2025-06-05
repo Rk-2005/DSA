f24bd3a91c3fa206fe5b28f9b7bbfd1e0386be05
Ritesh Kriplani <kriplanira@rknec.edu>
2025-06-06 00:56:04 +0530

    Time: 3 ms (41.19%), Space: 8.9 MB (65.86%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
-    char DFS(char ch, unordered_map<char, vector<char>>& mp,
-             vector<bool>& vis) {
-        vis[ch - 'a'] = true;
-        char c = ch;
-        for (auto i : mp[ch]) {
-            if (vis[i - 'a'] == 0) {
-                c = min(c, DFS(i, mp, vis));
-            }
+    vector<int> parent;
+    vector<int> rank;
+    int parentt(int node) {
+        if (parent[node] == node) {
+            return node;
         }
 
-        return c;
+        return parentt(parent[node]);
+    }
+    void unionn(int node1, int node2) {
+        int p1 = parentt(node1);
+        int p2 = parentt(node2);
+
+        if (p1<p2) {
+            rank[p1]++;
+            parent[p2] = p1;
+        } else {
+            rank[p2]++;
+            parent[p1] = p2;
+        }
     }
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        unordered_map<char, vector<char>> mp;
-        for (int i = 0; i < size(s1); i++) {
-            mp[s1[i]].push_back(s2[i]);
-            mp[s2[i]].push_back(s1[i]);
+        parent.resize(26);
+        rank.resize(26);
+        for (int i = 0; i < 26; i++) {
+            parent[i] = i;
         }
-        string res = "";
-        for (int i = 0; i < size(baseStr); i++) {
-            char ch = baseStr[i];
-            vector<bool> vis(26, 0);
-           // ab = ch;
-            res.push_back(min(ch, DFS(ch, mp, vis)));
+        for (int i = 0; i < s1.size(); i++) {
+            char ch1 = s1[i] - 'a';
+            char ch2 = s2[i] - 'a';
+            unionn(ch1, ch2);
         }
-        return res;
+        for (int i = 0; i < 26; i++) {
+            parent[i] = parentt(i);
+        }
+
+        for(int i=0;i<baseStr.size();i++){
+            int no1=(baseStr[i]-'a');
+            char ch1=char(parent[no1]+'a');
+          //  cout<<ch1<<" ";
+            baseStr[i]=min(baseStr[i],ch1);
+        }
+        //cout<<baseStr<<" ";
+
+        return baseStr;
     }
 };
