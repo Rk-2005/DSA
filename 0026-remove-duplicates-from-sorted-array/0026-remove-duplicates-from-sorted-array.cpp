85c6bc31986afded983e1362e969970418daafef
Ritesh Kriplani <kriplanira@rknec.edu>
2025-06-21 02:13:25 +0530

    Update 0026-remove-duplicates-from-sorted-array.cpp

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int count = 0;
         int size = nums.size();
-        for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
             int num = nums[i];
              while (i+1 < size && num == nums[i + 1]) { 
                 i++;
