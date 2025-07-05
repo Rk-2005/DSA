14dc87051dba79a7ca6d97f6105e8c8fe41a4592
Ritesh Kriplani <kriplanira@rknec.edu>
2025-07-06 00:58:37 +0530

    Time: 727 ms (6.25%), Space: 318.4 MB (6.25%) - LeetHub

---
+++ 3916-minimum-time-to-reach-destination-in-directed-graph/3916-minimum-time-to-reach-destination-in-directed-graph.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int N;
+    int solve(map<int, vector<int>>& mp, int node, int dest,
+              map<pair<int, int>, vector<pair<int, int>>> &costs) {
+
+        queue<pair<int, int>> q;
+
+        vector<int> visited(N, INT_MAX);
+
+        q.push({0, 0});
+
+        int ans = INT_MAX;
+
+        while (!q.empty()) {
+            int sz = q.size();
+
+            while (sz--) {
+                auto it = q.front();
+                q.pop();
+
+                int cur = it.first;
+                int curcost = it.second;
+
+                if (cur == dest) {
+                    ans = min(ans, curcost);
+                }
+
+                for (auto& child : mp[cur]) {
+
+                    auto totaltime = costs[{cur, child}];
+                    // vector<pair<int,int>>totaltime
+                    
+                    for(auto &pairr:totaltime){
+                        int x=pairr.first;
+                        int y=pairr.second;   
+                        if(y<curcost){
+                            continue;
+                        }
+                        int costy=max(curcost,x)+1;
+
+                        if(visited[child]>costy){
+                            visited[child]=costy;
+                            q.push({child,costy});
+                        }
+                    }
+                }
+            }
+        }
+        return ans == INT_MAX ? -1 : ans;
+    }
+
+    int minTime(int n, vector<vector<int>>& edges) {
+        map<int, vector<int>> mp;
+        map<pair<int, int>, vector<pair<int, int>>> costs;
+        N = n;
+        for (auto it : edges) {
+            int x = it[0];
+            int y = it[1];
+            int from = it[2];
+            int to = it[3];
+
+            if (costs.find({from, to}) == costs.end()) {
+                mp[x].push_back(y);
+            }
+
+            costs[{x, y}].push_back({from, to});
+        }
+
+        return solve(mp, 0, n - 1, costs);
+    }
+};
