fb0178bcebc5aaf9ff3cf2a3a92ea9d60c2cb6fe
Ritesh Kriplani <kriplanira@rknec.edu>
2025-07-08 20:36:30 +0530

    Time: 63 ms (54.3%), Space: 75 MB (39.16%) - LeetHub

---
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        int n = events.size();
+
+        sort(begin(events), end(events)); // [4,5],[1,5]
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        int i = 0;
+        int d = events[i][0];
+        int ans = 0;
+        while (!pq.empty() || i < n) {
+
+            while (i < n && d == events[i][0]) {
+                pq.push(events[i][1]);
+                i++;
+            }
+
+            while(!pq.empty() && pq.top()<d){
+                pq.pop();
+            }
+            if (!pq.empty()) {
+                pq.pop();
+                ans++;
+            }
+
+            d++;
+        }
+        return ans;
+    }
+};
