c93d39d507a042259bcf36276dd84cae9a123f22
Ritesh Kriplani <kriplanira@rknec.edu>
2025-07-13 21:08:13 +0530

    Time: 27 ms (77.62%), Space: 80.4 MB (24.63%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-    bool check(vector<int>& players,vector<int>& trainer,int target){
+    int check(vector<int>& players, vector<int>& trainer) {
         int n = players.size();
         int m = trainer.size();
         int i = 0;
         int j = 0;
 
-        while(i<n && j<m){
-            if(players[i]<=trainer[j]){
+        while (i < n && j < m) {
+            if (players[i] <= trainer[j]) {
                 i++;
                 j++;
-            }else{
+            } else {
                 j++;
             }
         }
-        return i<=target;
+        return i;
     }
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
         int n = players.size();
-        
-        int s=0;
-        int e=min(trainers.size(),players.size());
-        sort(begin(players),end(players));
-        sort(begin(trainers),end(trainers));
-        int ans=0;
 
-        while(s<=e){
-            int mid  = s+(e-s)/2;
+        sort(begin(players), end(players));
+        sort(begin(trainers), end(trainers));
+        int ans = 0;
 
-            if(check(players,trainers,mid)){
-                ans=mid;
-                e=mid-1;
-            }else{
-                s=mid+1;
-            }
-        }
-        return ans;
+        return check(players, trainers);
     }
 };
 // 0
 // 1
-// 2 
+// 2
 // 3
-// 4 
-// 5 
+// 4
+// 5
 // 6
 // [3,4] , [5,2,3]
 // 3>2 = > not pos
