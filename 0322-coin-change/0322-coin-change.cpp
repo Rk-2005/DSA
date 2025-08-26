14ca4ff9e1278aee7b60872f405f4d25b3aacd6e
Ritesh Kriplani <kriplanira@rknec.edu>
2025-08-27 01:28:14 +0530

    Time: 59 ms (15.46%), Space: 89.7 MB (5%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,39 +1,29 @@
-
 class Solution {
 public:
-    int dp[13][10004];
-    long long solve(vector<int>& coins, int amount, int n, int idx) {
-        // Base case: if we have considered all coins
-        if (idx >= n) {
-            if (amount == 0) {
-                return 0;
-            }
-            return INT_MAX;
-        }
-        if(dp[idx][amount]!=-1)return dp[idx][amount];
-
-        // If the amount becomes negative, it's not valid
-        if (amount < 0)
-            return INT_MAX;
-
-        // Try to take the current coin
-        long long take = INT_MAX;
-        if (coins[idx] <= amount) {
-            take = 1+ solve(coins, amount - coins[idx], n, idx);
-        }
-
-        // Try not to take the current coin
-        long long nottake = solve(coins, amount, n, idx + 1);
-
-        // Return the minimum of taking or not taking the coin
-        return dp[idx][amount]= min(take, nottake);
-    }
-
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int k) {
         int n = coins.size();
-        
-        memset(dp,-1,sizeof(dp));
-        // If no solution is found, return -1
-        return ( solve(coins, amount, n, 0) == INT_MAX) ? -1 :  solve(coins, amount, n, 0);;
+        vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, INT_MAX));
+
+        for (int i = 0; i <= k; i++) {
+            if (i % coins[0] == 0) {
+                dp[0][i] = i / coins[0];
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            long long num = coins[i];
+
+            for (int j = 0; j <= k; j++) {
+
+                dp[i][j] = dp[i - 1][j];
+
+                if(j>=num){
+                    dp[i][j] = min(dp[i][j],1+dp[i][j-num]);
+                }
+
+            }
+        }
+        return (dp[n - 1][k] == INT_MAX ? -1 : dp[n - 1][k]);
     }
 };
+// dp[]
