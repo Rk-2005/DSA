b8396fd2495ebcfc84f3bd1cd2d59edc716c88e1
Ritesh Kriplani <ronakkriplani9@gmail.com>
2026-01-12 01:33:31 +0530

    Time: 283 ms (5%), Space: 21.4 MB (14.63%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,70 +1,112 @@
 class Solution {
 public:
-    // 1 2 6 6
-    vector<int> prev_smallest(vector<int>& col_sum) {
-        int n = size(col_sum);
-        vector<int> ans(n);
-        stack<int> st;
-        st.push(-1);
-        ans[0] = -1;
-        for (int i = 0; i < n; i++) {
-            while (st.top() != -1 && col_sum[st.top()] >= col_sum[i]) {
-                st.pop();
-            }
-            ans[i] = st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int> nxt_smallest(vector<int>& col_sum) {
-        int n = size(col_sum);
-        vector<int> ans(n);
-        stack<int> st;
-        st.push(n);
-        ans[n - 1] = n;
-        for (int i = n - 1; i >= 0; i --) {
-            while (st.top() != n && col_sum[st.top()] >= col_sum[i]) {
-                st.pop();
-            }
-            ans[i] = st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-    // 5 6 1
-    int find_ans(vector<int>& col_sum) {
-        int n = size(col_sum);
-        vector<int> nxts = nxt_smallest(col_sum);
-        vector<int> prevs = prev_smallest(col_sum);
-        for(auto it:nxts)cout<<it<<" ";
-        cout<<endl;
-        for(auto it:prevs)cout<<it<<" ";
-        int ans = 0;
-        for (int i = 0; i < col_sum.size(); i++) {
+    vector<int> FindPrevious(vector<int>& array) {
+        int n = array.size();
 
-            ans = max(ans, col_sum[i] * (nxts[i] - prevs[i] - 1));
+        stack<int> st;
+
+        st.push(-1);
+
+        vector<int> ans(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            while (st.top() != -1 && array[st.top()] >= array[i]) {
+                st.pop();
+            }
+
+            ans[i] = st.top();
+            st.push(i);
         }
         return ans;
+    }
+
+    vector<int> FindNext(vector<int>& array) {
+        int n = array.size();
+
+        stack<int> st;
+
+        st.push(n);
+
+        vector<int> ans(n, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.top() != n && array[st.top()] >= array[i]) { // 3 4 2
+                st.pop();
+            }
+
+            ans[i] = st.top();
+            st.push(i);
+        }
+
+        return ans;
+    }
+    int FindAns(vector<int>& array) {
+        int sz = array.size();
+
+        vector<int> left = FindPrevious(array);
+        vector<int> right = FindNext(array);
+
+        int MaxAns = 0;
+
+        for (int i = 0; i < sz; i++) {
+            int height = array[i];
+
+            int width = right[i] - left[i] - 1;
+
+            int currentAns = height * width;
+
+            MaxAns = max(MaxAns, currentAns);
+        }
+
+        return MaxAns;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-      
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<int> col_sum(m);
-        for (int i = 0; i < m; i++) {
-            col_sum[i] =( (matrix[0][i] - '0'));
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        vector<int> cols(col, 0);
+
+        for (int i = 0; i < col; i++) {
+            if (matrix[0][i] == '1') {
+                cols[i] = 1;
+            } else {
+                cols[i] = 0;
+            }
         }
-        int ans = find_ans(col_sum);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == '0') {
-                    col_sum[j] = 0;
+
+        int MaxAns = FindAns(cols);
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    cols[j] += 1;
                 } else {
-                    col_sum[j] += 1;
+                    cols[j] = 0;
                 }
             }
-            ans = max(ans, find_ans(col_sum));
+            for(auto it:cols){
+                cout<<it<<" ";
+            }
+            cout<<endl;
+            MaxAns = max(MaxAns, FindAns(cols));
         }
-        return ans;
+
+        return MaxAns;
     }
 };
+
+// 1 0 1 0 0
+
+// 0 1 2 3 4 5
+// 1 4 2 2 3 1
+
+// 0 1 2 3 4 5
+// 4 2 2 3 1 1
+
+// height * () =
+
+// 1 4 3 2
+
+// 1 4 3 2
+// -1 , 0 ,
